642359b84245de579fbf10c4ccdc21aad814d4c8
Spencer <spencerberenson@gmail.com>
2016-12-06 12:42:10 -0500

    Better modals.

--- _harp/index.js
+++ _harp/index.js
@@ -1,4 +1,4 @@
 $(".card-image").click(function() {
-	let modal = $(`<div class="modal is-active"><div class="modal-background"><div class="modal-content">${$(this).html()}</div><div class="modal-close"></div></div></div>`);
-	modal.appendTo("body").find(".modal-background").click(function() { $(this).parent().remove(); });
+	let modal = $(`<div class="modal is-active"><div class="modal-background"></div><div class="modal-content">${$(this).html() + $(this).parent().find(".card-content").html()}</div><button class="modal-close"></button></div>`);
+	modal.appendTo("body").find(".modal-background, .modal-close").click(function() { $(this).parent().remove(); });
 });
