67cb4af28b3d6f5022ec583a8cb658c572e4616e
Spencer <spencerberenson@gmail.com>
2016-12-06 12:01:46 -0500

    Fixed issue with closing modal.

--- _harp/index.js
+++ _harp/index.js
@@ -1,4 +1,4 @@
 $(".card-image").click(function() {
 	let modal = $(`<div class="modal is-active"><div class="modal-background"><div class="modal-content">${$(this).html()}</div><div class="modal-close"></div></div></div>`);
-	modal.appendTo("body").find(".modal-background, .modal-close").click(function() { $(this).parent().remove(); });
+	modal.appendTo("body").find(".modal-background").click(function() { $(this).parent().remove(); });
 });
